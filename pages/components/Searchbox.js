50aafb1e939ebdfe29299574b3b0329a778625d2
Altay Simsek <altaysimsek16@gmail.com>
2020-11-29 13:11:24 +0300

    Added searchbox component to index.js

--- pages/components/Searchbox.js
+++ pages/components/Searchbox.js
@@ -2,10 +2,43 @@ import styles from "../../styles/Searchbox.module.scss";
 
 function Searchbox() {
     return (
-        <>
-            
-        </>
+        <div className={styles.searchbox}>
+            <div className={styles.selectbox}>
+                <select id="years" name="years">
+                    <option value="" disabled selected hidden>
+                        Year
+                    </option>
+                    <option value="2020">2020</option>
+                    <option value="2019">2019</option>
+                    <option value="2018">2018</option>
+                    <option value="2017">2017</option>
+                </select>
+            </div>
+            <div className={styles.selectbox}>
+                <select id="types" name="types">
+                    <option value="" disabled selected hidden>
+                        Type
+                    </option>
+                    <option value="Action">Action</option>
+                    <option value="Sci-fi">Sci-fi</option>
+                    <option value="Adventure">Adventure</option>
+                    <option value="Romantic">Romantic</option>
+                </select>
+            </div>
+            <div className={styles.inputbox}>
+                <input
+                    type="text"
+                    placeholder="Enter movie name here"
+                    className="mr-5"
+                    name="searchbox"
+                ></input>
+                <i class="bx bx-search"></i>
+            </div>
+            <button className={styles.searchButton}><span>
+                Search
+                </span><i class='bx bx-right-arrow-alt'></i></button>
+        </div>
     );
 }
 
-export default Navigation;
+export default Searchbox;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
-import { Container} from "react-bootstrap";
-import styles from "../styles/Home.module.scss"
+import { Container } from "react-bootstrap";
+import Searchbox from "./components/Searchbox";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
     return (
@@ -10,8 +11,24 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Container fluid className={styles.branding}>
-              <h1>Welcome to <br></br> <b>MovieUP.</b></h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
+                <h1>
+                    Welcome to <br></br> <b>MovieUP.</b>
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris{" "}
+                </p>
+            </Container>
+            <Container>
+                <div style={{ marginTop: "-3rem" }}>
+                    <Searchbox></Searchbox>
+                </div>
+                <div className={styles.popularMovies}>
+                  <h2>Popular Movies</h2>
+                  <span>View more <i class='bx bx-right-arrow-alt' ></i></span>
+                </div>
             </Container>
         </>
     );
